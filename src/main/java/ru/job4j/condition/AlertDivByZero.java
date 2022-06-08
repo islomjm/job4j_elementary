f7a218fbbe4dad0e7d6f1b5130071339d7805cc6
islom <islomnizom@gmail.com>
2022-06-08 23:59:11 +0300

    4.1. Операторы ветвлений [#3611]

---
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+    public static void main(String[] args) {
+        possibleDiv(4);
+        possibleDiv(0);
+    }
+
+    public static void possibleDiv(int number) {
+        if (number == 0) {
+            System.out.println("Число 0 делить нельзя.");
+        }
+        if (number != 0) {
+            System.out.println("Это отрицательное число.");
+        }
+    }
+}
