45c9bbf04b31da73f3e53a771240096882fcdbf1
islomjon <islomnnizom@gmail.com>
2022-06-09 17:12:38 +0300

    Type

--- src/main/java/ru/job4j/calculator/Type.java
+++ src/main/java/ru/job4j/calculator/Type.java
@@ -9,6 +9,7 @@ public class Type {
         System.out.println();
         float f = 12358.7f;
         int i = 45981;
+
         short rsl = (short) (f + i);
         System.out.println("float = " + f);
         System.out.println("int = " + i);
