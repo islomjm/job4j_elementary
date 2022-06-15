ed2ad2d1955b51fb3d3b41213d88a3e311e1c2ca
islom <islomnizom@gmail.com>
2022-06-15 10:32:32 +0300

    5.5. Простое число [#171694]

--- src/main/java/ru/job4j/condition/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/condition/CheckPrimeNumber.java
@@ -2,11 +2,13 @@ package ru.job4j.condition;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        for (int i = 2; number <= -1; i++) {
+        boolean prime = number > 1;
+        for (int i = 2; i <= number / 2; i++) {
             if ((number % i) == 0) {
+                prime = false;
                 break;
             }
+
         }
         return prime;
     }
