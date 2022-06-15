5c761e29bd846bf66f7800795d993ebf39103d94
islom <islomnizom@gmail.com>
2022-06-15 11:59:04 +0300

    5.5. Простое число [#171694]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,4 +1,4 @@
-package ru.job4j.condition;
+package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
--- src/test/java/ru/job4j/condition/CheckPrimeNumberTest.java
+++ src/test/java/ru/job4j/condition/CheckPrimeNumberTest.java
@@ -2,6 +2,7 @@ package ru.job4j.condition;
 
 import org.junit.Assert;
 import org.junit.Test;
+import ru.job4j.loop.CheckPrimeNumber;
 
 public class CheckPrimeNumberTest {
 
