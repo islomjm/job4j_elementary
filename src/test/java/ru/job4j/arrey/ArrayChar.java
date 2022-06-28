f8572596014ff423739910b08c6ac84510ffce3f
legion <islomnizom@gmail.com>
2022-06-28 15:04:28 +0300

    6.4.0. Слово начинается с ... [#41585]

---
+++ src/test/java/ru/job4j/arrey/ArrayChar.java
@@ -0,0 +1,14 @@
+package ru.job4j.arrey;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (char i = 0; i < pref.length; i++) {
+            if (word[i] != pref[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
