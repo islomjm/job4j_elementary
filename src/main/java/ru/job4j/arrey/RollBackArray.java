75c464c73abf5775a6b60f5e16b85666e9d46917
legion <islomnizom@gmail.com>
2022-06-29 08:32:23 +0300

    6.4.2. Обход массива с последнего элемента [#361619]

---
+++ src/main/java/ru/job4j/arrey/RollBackArray.java
@@ -0,0 +1,13 @@
+package ru.job4j.arrey;
+
+public class RollBackArray {
+    public static int[] rollback(int[] array) {
+        int[] result = new int[array.length];
+        for (int index = 0; index < array.length; index++) {
+            int lastIndex = array.length - 1 - index;
+            int currentIndex = lastIndex;
+            result[currentIndex] = array[index];
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/arrey/RollBackArrayTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.arrey;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RollBackArrayTest {
+
+    @Test
+    public void whenEmpty() {
+        int[] input = new int[] {};
+        int[] expected = new int[] {};
+        int[] result = RollBackArray.rollback(input);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenOne() {
+        int[] input = new int[] {1};
+        int[] expected = new int[] {1};
+        int[] result = RollBackArray.rollback(input);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenFull() {
+        int[] input = new int[] {1, 2, 3, 4};
+        int[] expected = new int[] {4, 3, 2, 1};
+        int[] result = RollBackArray.rollback(input);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenTheSame() {
+        int[] input = new int[] {1, 1, 1, 1};
+        int[] expected = new int[] {1, 1, 1, 1};
+        int[] result = RollBackArray.rollback(input);
+        assertArrayEquals(expected, result);
+    }
+}
