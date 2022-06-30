5f1c5f83e08e0a02462de0673e52a38bad5c1ebd
legion <islomnizom@gmail.com>
2022-06-30 21:49:25 +0300

    6.4.3. Слово заканчивается на ... [#156317 #317122]

--- src/main/java/ru/job4j/arrey/EndsWith.java
+++ src/main/java/ru/job4j/arrey/EndsWith.java
@@ -2,6 +2,13 @@ package ru.job4j.arrey;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        return word[word.length - 1] == post[post.length - 1];
+        boolean result = true;
+        for (int i = 0; i < post.length; i++) {
+            if (word[word.length - i - 1] != post[post.length - i - 1]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
--- src/test/java/ru/job4j/arrey/EndWithTest.java
+++ src/test/java/ru/job4j/arrey/EndWithTest.java
@@ -3,8 +3,6 @@ package ru.job4j.arrey;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class EndWithTest {
 
     @Test
