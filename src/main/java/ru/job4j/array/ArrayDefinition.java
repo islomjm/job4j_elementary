96283b69510f7ed5b8661cb3b7a63cc71c56acd4
islom <islomnizom@gmail.com>
2022-08-07 07:50:06 +0300

    array

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,10 +3,10 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         String[] names = new String[4];
-        names[0] = "Kirill Kalakin";
-        names[1] = "Andrey Kalakin";
-        names[2] = "Larisa Kalakina";
-        names[3] = "Georgy Kalakin";
+        names[0] = "Iroda";
+        names[1] = "Andrey";
+        names[2] = "Larisa";
+        names[3] = "Georgy";
         for (int index = 0; index < names.length; index++) {
             System.out.println(names[index]);
         }
